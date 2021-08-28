69a80b74c61a4147a043c909dcb8dd84f049389f
AshwiniSetty <ashwinisetty@hotmail.com>
2021-08-28 23:10:10 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check");
+const displayMessage = document.querySelector("#output-container");
+
+const calculateSum = (date) => {
+  let sum = 0;
+  date = date.replaceAll("-", "");
+  for (let digit of date) {
+    sum = sum + Number(digit);
+  }
+  return sum;
+};
+
+const checkIsNumberLucky = (sumOfDate, numberToCheck) => {
+  console.log(sumOfDate, numberToCheck);
+  if (sumOfDate % numberToCheck === 0) {
+    return showMessage(`Yayyy!🥳 ${numberToCheck} is a lucky number for you!!`);
+  }
+  showMessage(`Oh No!🙁 ${numberToCheck} is not that lucky for you`);
+};
+
+const showMessage = (message) => {
+  displayMessage.innerText = message;
+};
+
+checkButton.addEventListener("click", () => {
+  const date = dateOfBirth.value;
+  const numberToCheck = luckyNumber.value;
+  if (date && numberToCheck) {
+    const sumOfDate = calculateSum(date);
+    checkIsNumberLucky(sumOfDate, numberToCheck);
+  } else {
+    showMessage("🙄Please enter both the fields");
+  }
+});
